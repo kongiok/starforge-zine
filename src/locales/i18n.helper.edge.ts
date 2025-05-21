955889ccf1fe046ba42daf2d0c8db9208eceaaf1
kongiok <ong.kongiok@gmail.com>
2025-05-22 01:52:21 +0800

    feat: add multi lang support for this site

--- src/locales/i18n.helper.edge.ts
+++ src/locales/i18n.helper.edge.ts
@@ -1,8 +1,9 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 import { parse } from "@iarna/toml";
+import { I18nDictionary, I18nDictionarySchema } from "./i18n.type";
 
-export const loadLocale = (code: string): Record<string, any> => {
+export const loadLocale = (code: string): I18nDictionary => {
   const filePath = join(
     process.cwd(),
     "src",
@@ -10,7 +11,9 @@ export const loadLocale = (code: string): Record<string, any> => {
     `dictionaries/${code}.toml`,
   );
   const raw = readFileSync(filePath, "utf-8");
-  return parse(raw);
+  const parsed = parse(raw);
+
+  return I18nDictionarySchema.parse(parsed);
 };
 
 export const getTranslator = (code: string) => {
@@ -18,14 +21,16 @@ export const getTranslator = (code: string) => {
 
   const t = <T = string | string[]>(key: string): T => {
     const keys = key.split(".");
-    let result: any = dict;
+    let result: unknown = dict;
 
     for (const k of keys) {
-      result = result?.[k];
-      if (result === undefined) return key as any;
+      if (typeof result !== "object" || result === null) {
+        return key as T;
+      }
+      result = (result as Record<string, unknown>)[k];
     }
 
-    return result;
+    return (result ?? key) as T;
   };
 
   return t;
